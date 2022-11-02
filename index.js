65c444560fd55d1163321924d1086849008c3cfe
jyiwm <joshua.yi@warnermedia.com>
2022-11-02 14:23:37 -0400

    Add workflow for dependecies. Remove node_modules. Add code for action

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { context } = github;
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const jiraAccount = core.getInput('jira-account');
+  const ticketRegex = new RegExp(core.getInput('ticket-regex') || '[A-Z]+-\\d+');
+  const headBranch = context.payload.pull_request.head.ref;
+  const [ticketId] = headBranch.match(ticketRegex) || []
+
+  const url = `https://${jiraAccount}.atlassian.net/browse/${ticketId}`;
+
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
