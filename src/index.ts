19a456aae88da59c509b22d7308488e3d720c188
jyiwm <joshua.yi@warnermedia.com>
2022-11-07 12:05:45 -0500

    Update URL body and make request

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 /* global console */
 import * as core from '@actions/core';
-import { context } from '@actions/github';
+import { context, getOctokit } from '@actions/github';
 
 try {
   // `who-to-greet` input defined in action metadata file
   // const nameToGreet = core.getInput('who-to-greet');
+  const token = core.getInput('github-token');
   const jiraAccount = core.getInput('jira-account');
   const ticketRegex = new RegExp(core.getInput('ticket-regex') || '[A-Z]+-\\d+');
-  const headBranch = context.payload.pull_request.head.ref;
-  const [ticketId] = headBranch.match(ticketRegex) || ['GOONIES-123'];
 
+  const { repo } = context;
+  const { pull_request } = context.payload;
+  const prNumber = pull_request.number;
+  const headBranch = pull_request.head.ref;
+
+  // Create URL for body
+  const [ticketId] = headBranch.match(ticketRegex) || ['GOONIES-123'];
   const url = `https://${jiraAccount}.atlassian.net/browse/${ticketId}`;
+
+  // Make request to update PR
+  const octokit = getOctokit(token);
+  const request: Parameters<typeof octokit.rest.pulls.update>[0] = {
+    body: url,
+    owner: repo.owner,
+    repo: repo.repo,
+    pull_number: prNumber,
+  }
+
+  const response = await octokit.rest.pulls.update(request);
+
+  if (response.status !== 200) {
+    core.error(`PR Update failed with ${response.status}`);
+  }
 
   // console.log(`Hello ${nameToGreet}!`);
   // const time = (new Date()).toTimeString();
